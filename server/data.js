59b1e7c692393199f5136cb2b27db61ce3b5f7f9
Sayantan Hore <sayantan.hore.86@gmail.com>
2016-11-14 23:27:42 +0200

    Dummy data added

--- server/data.js
+++ server/data.js
@@ -71,6 +71,24 @@ module.exports.getData = function(param) {
     "createdtime":"2016-11-10T16:53:05.995Z",
     "endtime":"2016-11-10T16:53:11.035Z",
     "duration":5.04,"format_name":"mp3"
+  }, {
+    "transcription":"then i looked down and i realize there is a bonus at",
+    "filename":"193b6584da306f1c593566a19465cb23",
+    "createdtime":"2016-11-09T21:52:18.570Z",
+    "endtime":"2016-11-09T21:52:23.610Z",
+    "duration":5.04,"format_name":"mp3"
+  }, {
+    "transcription":"and I look down and I realize there is a phone",
+    "filename":"fcefab3e663d5107180abd6c06742ac5",
+    "createdtime":"2016-11-10T16:53:05.995Z",
+    "endtime":"2016-11-10T16:53:11.035Z",
+    "duration":5.04,"format_name":"mp3"
+  }, {
+    "transcription":"and I look down and I realize there is a phone",
+    "filename":"fcefab3e663d5107180abd6c06742ac5",
+    "createdtime":"2016-11-10T16:53:05.995Z",
+    "endtime":"2016-11-10T16:53:11.035Z",
+    "duration":5.04,"format_name":"mp3"
   }];
 
   return data;
