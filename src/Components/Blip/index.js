3cc104e3c5e30056df8a2fb29d9b7b451512de58
Sayantan Hore <sayantan.hore.86@gmail.com>
2016-11-21 16:15:51 +0200

    Blip header modified to readable

--- src/Components/Blip/index.js
+++ src/Components/Blip/index.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react';
 import Listview from './Listview';
 import $ from 'jquery';
+import moment from 'moment';
 require('./Blip.scss');
 
 export default class Blip extends Component {
   constructor() {
     super();
+    this.months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
     this.state = {listView: false};
+    this.getMonthName = this.getMonthName.bind(this);
     this.showListview = this.showListview.bind(this);
     this.closeListview = this.closeListview.bind(this);
     this.playAudio = this.playAudio.bind(this);
+  }
+
+  getMonthName(month) {
+    return this.months[month];
   }
 
   showListview(direction) {
@@ -52,8 +72,16 @@ export default class Blip extends Component {
         blipView = (
           <div>
             <div className="div__blip__header">
-              <span>
-                {this.props.data.createdtime}
+              <span className="span__year">
+
+                {moment(this.props.data.createdtime).format('YYYY')}
+
+              </span>
+              <span className="span__dayandmonth">
+                {moment(this.props.data.createdtime).format('ddd, MMM Do') + ' '}
+              </span>
+              <span className="span__time">
+                {moment(this.props.data.createdtime).format('h:mm a') + ' '}
               </span>
             </div>
             <div className="div__blip__body">
