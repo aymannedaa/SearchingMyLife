95d5382c66e07af47972713d72c58d3b770e9898
Sayantan Hore <sayantan.hore.86@gmail.com>
2016-11-23 20:54:48 +0200

    Logout call implemented

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -5,7 +5,21 @@ require('./Header.scss');
 export default class Header extends Component {
   constructor() {
     super();
+    this.logoutHandler = this.logoutHandler.bind(this);
   }
+
+  logoutHandler() {
+    console.log('log out')
+    $.ajax({
+        url: '/logout',
+        type: 'GET',
+      }).done((data) => {
+
+      }).fail((err) => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <div className="div__header__container">
@@ -13,7 +27,7 @@ export default class Header extends Component {
           <span className="span-text-item">Searching My Life</span>
         </div>
         <div className="div__header__items">
-          {this.props.username} <span className="span-text-item link">Logout</span>
+          {this.props.username} <span className="span-text-item link" onClick={this.logoutHandler}>Logout</span>
         </div>
       </div>
     );
