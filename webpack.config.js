ca1faa5ad56be89328206d257deb3cad31a6448c
Sayantan Hore <sayantan.hore.86@gmail.com>
2016-11-23 21:11:02 +0200

    Production changes

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ const nodeModulesPath = path.resolve(__dirname, 'node_modules');
 const srcPath = path.resolve(__dirname, 'src', 'app.js');
 const distPath = path.resolve(__dirname, 'dist');
 
-const production = false;
+const production = true;
 
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack/hot/dev-server',
-    'webpack-dev-server/client?http://localhost:8888',
+    //'webpack/hot/dev-server',
+    //'webpack-dev-server/client?http://localhost:8888',
     srcPath
   ],
   output: {
@@ -29,8 +29,8 @@ module.exports = {
       }
     }, {
       test: /.scss$/,
-      loaders: ['style-loader', 'css-loader', 'sass-loader']
-      //loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+      //loaders: ['style-loader', 'css-loader', 'sass-loader']
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
     }]
   },
   plugins: production ? [
