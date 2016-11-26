2d54aca08ccf133a9fea58ac9d5eb5362a1937f7
Sayantan Hore <sayantan.hore.86@gmail.com>
2016-11-26 23:45:32 +0200

    Images shown along with text

--- src/Components/Blip/index.js
+++ src/Components/Blip/index.js
@@ -1,38 +1,67 @@
 import React, {Component} from 'react';
 import Listview from './Listview';
 import $ from 'jquery';
+import _ from 'lodash';
 import moment from 'moment';
 require('./Blip.scss');
+
+
+function openModal(event) {
+    const src = $(event.target).attr('src');
+    const largeImage = $('<img src=' + src + '></img>');
+    const closeIcon = $('<img class="close" src="icons/close.svg"></img>');
+    closeIcon.on('click', function() {
+      $('.div__modal').remove();
+    });
+    const modalDiv = $('<div class="div__modal"></div>');
+    const modalHeaderDiv = $('<div class="div__modal__header"></div>');
+
+    modalDiv.appendTo('body');
+    modalHeaderDiv.appendTo(modalDiv);
+    closeIcon.appendTo(modalHeaderDiv);
+    largeImage.appendTo(modalDiv);
+  }
+
+
 
 export default class Blip extends Component {
   constructor() {
     super();
-    this.months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December'
-    ];
-    this.state = {listView: false};
-    this.getMonthName = this.getMonthName.bind(this);
-    this.showListview = this.showListview.bind(this);
+    this.state = {listView: false, activeTab: 'tabText'};
+    this.showListviewText = this.showListviewText.bind(this);
+    this.showListviewImage = this.showListviewImage.bind(this);
     this.closeListview = this.closeListview.bind(this);
     this.playAudio = this.playAudio.bind(this);
+
+    this.setAsActiveTab = this.setAsActiveTab.bind(this);
+    this.fetchImageMetadata = this.fetchImageMetadata.bind(this);
+    this.resizeImage = this.resizeImage.bind(this);
   }
 
-  getMonthName(month) {
-    return this.months[month];
+  showListviewImage(direction) {
+    let url = '';
+    let timestamp = null;
+    if(direction === 'back') {
+      timestamp = this.props.data.createdtime;
+      url = '/search/image/before/' + timestamp;
+    }
+    else if(direction === 'next') {
+      timestamp = this.props.data.endtime;
+      url = '/search/image/after/' + timestamp;
+    }
+    $.ajax({
+      url: url,
+      type: 'GET',
+    }).done((data) => {
+      //store.setData(data);
+      //that.props.update();
+      this.setState({listView: true, listData: data, direction: direction, timestamp: timestamp});
+    }).fail((err) => {
+      console.log(err);
+    });
   }
 
-  showListview(direction) {
+  showListviewText(direction) {
     const word = this.props.searchingFor;
     let url = '';
     let timestamp = null;
@@ -57,6 +86,28 @@ export default class Blip extends Component {
 
   }
 
+  fetchImageMetadata() {
+    const url = '/search/image/between/' + this.props.data.createdtime + '/' + this.props.data.endtime;
+    $.ajax({
+      url: url,
+      type: 'GET'
+    }).done((data) => {
+      const imageFilenames = _.map(data, (item) => item.filename);
+      this.setState({imageFilenames: imageFilenames, activeTab: 'tabImage'});
+    }).fail((err) => {
+      console.log(err);
+    })
+  }
+
+  setAsActiveTab(event) {
+    if((this.refs.tab_text === event.target) || (this.refs.tab_text === $(event.target).closest('div').get(0))) {
+      this.setState({activeTab: 'tabText'});
+    }
+    else if((this.refs.tab_image === event.target) || (this.refs.tab_image === $(event.target).closest('div').get(0))) {
+      this.fetchImageMetadata();
+    }
+  }
+
   closeListview() {
     this.setState({listView: false, listData: null, direction: null, timestamp: null});
   }
@@ -67,6 +118,13 @@ export default class Blip extends Component {
       duration: this.props.data.duration
     });
   }
+
+  resizeImage(event) {
+    const parentWidth = $(event.target).parent().css('width').replace("px", "");
+    const widthToSet = parseFloat(parentWidth) / 6;
+    $(event.target).css('width', widthToSet + 'px');
+  }
+
   render() {
     let blipView = null;
       if(!this.state.listView) {
@@ -84,18 +142,63 @@ export default class Blip extends Component {
               </span>
             </div>
             <div className="div__blip__body">
-              <p>
-                {this.props.data.transcription}
-              </p>
+              <div className="div__blip__body__tabs">
+                <div
+                  ref="tab_text"
+                  className={(this.state.activeTab === 'tabText') ? "div__active" : ""}
+                  onClick={this.setAsActiveTab}>
+                  <img className="img__text" src="icons/text.svg"></img>
+                </div>
+                <div
+                  ref="tab_image"
+                  className={(this.state.activeTab === 'tabImage') ? "div__active" : ""}
+                  onClick={this.setAsActiveTab}>
+                  <img className="img__image" src="icons/image.svg"></img>
+                </div>
+              </div>
+              <div className="div__blip__body__content">
+                {(this.state.activeTab === 'tabText') ?
+                  <p>
+                    {this.props.data.transcription}
+                  </p>
+                  :
+                  <div className="div__blip__body__content__image-gallery">
+                    {this.state.imageFilenames.map((filename, index) => {
+                      return <img
+                        key={index}
+                        src={'/sendImage/' + filename}
+                        onLoad={this.resizeImage}
+                        onClick={openModal}></img>;
+                    })}
+                  </div>
+                }
+              </div>
+
             </div>
             <div className="div__blip__ctrlbar">
-              <span className="span-text-item link" onClick={() => this.showListview('back')}>
+              <span className="span-text-item link" onClick={() => {
+                if(this.state.activeTab === 'tabText') {
+                  this.showListviewText('back')
+                }
+                else if(this.state.activeTab === 'tabImage') {
+                  this.showListviewImage('back');
+                }
+              }}>
                 Back
               </span>
-              <span className="span-text-item link" onClick={this.playAudio}>
+              <span
+                className={(this.state.activeTab === 'tabText') ? "span-text-item link" : "hidden"}
+                onClick={this.playAudio}>
                 Play
               </span>
-              <span className="span-text-item link" onClick={() => this.showListview('next')}>
+              <span className="span-text-item link" onClick={() => {
+                if(this.state.activeTab === 'tabText') {
+                  this.showListviewText('next')
+                }
+                else if(this.state.activeTab === 'tabImage') {
+                  this.showListviewImage('next');
+                }
+              }}>
                 Next
               </span>
             </div>
@@ -109,7 +212,8 @@ export default class Blip extends Component {
           play={this.props.play}
           close={this.closeListview}
           direction={this.state.direction}
-          timestamp={this.state.timestamp}></Listview>
+          timestamp={this.state.timestamp}
+          imageView={(this.state.activeTab === 'tabImage') ? true : false}></Listview>
         );
       }
 
